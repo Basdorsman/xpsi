151f1e82a35b6997cf542d6ec07d13140f1b72bb
Basdorsman <superbas1996@gmail.com>
2023-03-29 13:23:37 +0200

    modified helios job and sample.py to work for helios

--- tests/inference_run/helios_job.sh
+++ tests/inference_run/helios_job.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1
 #SBATCH --tasks-per-node=126
 #SBATCH -t 1-00:00:00
-#SBATCH -J example_run
-#SBATCH -o example_run_%j.out
-#SBATCH -e example_run_%j.err
+#SBATCH -J time_likelihood
+#SBATCH -o output/time_likelihood_%j.out
+#SBATCH -e output/time_likelihood_%j.err
 #SBATCH --partition=neutron-star
 #SBATCH --mem 160000
 #SBATCH --mail-user=
@@ -15,6 +15,11 @@ module load anaconda3/2021-05
 conda activate xpsi_py3
 module load openmpi/3.1.6
 
+export atmosphere_type="A"
+export n_params="5"
+export JOBNAME='time_likelihood'
+
+export RUN="run_${atmosphere_type}${n_params}" 
 export OMP_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export GOTO_NUM_THREADS=1
@@ -25,7 +30,7 @@ export PATH=$HOME/gsl/bin:$PATH
 export JOB_DIR=$HOME/xpsi-bas/tests/inference_run
 export OUTPUT_FOLDER=$(mktemp -d -p /hddstore/$USER)
 echo $OUTPUT_FOLDER $SLURMD_NODENAME
-mkdir $OUTPUT_FOLDER/run
+mkdir $OUTPUT_FOLDER/$RUN
 cd $OUTPUT_FOLDER
 
 #Copy the input data to be visible for all the nodes (and make sure your paths point to hddstore):
@@ -35,9 +40,10 @@ sleep 1
 mpiexec -n 126 -mca btl_tcp_if_include ib0 python $JOB_DIR/sample.py
 
 #Move your output from scratch to storage space.
-mkdir -p /zfs/helios/filer0/$USER/
-cp -r $OUTPUT_FOLDER/* /zfs/helios/filer0/$USER/
+mkdir -p /zfs/helios/filer0/$USER/$JOBNAME/$RUN/$SLURM_JOB_ID
+cp -r $OUTPUT_FOLDER/$RUN /zfs/helios/filer0/$USER/$JOBNAME/$RUN/$SLURM_JOB_ID
+cp $JOB_DIR/output/time_likelihood_${SLURM_JOB_ID}.* /zfs/helios/filer0/$USER/$JOBNAME/$RUN/$SLURM_JOB_ID
 
 #Clean the scratch automatically here.
 #But remember to remove manually in each node, if the main program ends by crashing.
-#rm -rf $OUTPUT_FOLDER
+rm -rf $OUTPUT_FOLDER
--- tests/inference_run/sample.py
+++ tests/inference_run/sample.py
@@ -19,7 +19,7 @@ from xpsi.global_imports import gravradius
 
 
 import sys
-sys.path.append('../')
+sys.path.append('/home/bdorsma/xpsi-bas/tests/')
 from custom_tools import CustomInstrument, CustomHotRegion, CustomHotRegion_Accreting, CustomHotRegion_Accreting_te_const
 from custom_tools import CustomPhotosphere_BB, CustomPhotosphere_N4, CustomPhotosphere_N5, CustomPhotosphere_A5, CustomPhotosphere_A4
 from custom_tools import CustomSignal, CustomPrior, CustomPrior_NoSecondary, plot_2D_pulse, CustomLikelihood
@@ -567,13 +567,13 @@ runtime_params = {'resume': False,
                   'n_clustering_params': None,
                   'outputfiles_basename': f'./{folderstring}/run_Num', 
                   'n_iter_before_update': 100,
-                  'n_live_points': 2,#2,#4000,
+                  'n_live_points': 50,#2,#4000,
                   'sampling_efficiency': 0.1,
                   'const_efficiency_mode': False,
                   'wrapped_params': wrapped_params,
                   'evidence_tolerance': 0.1,
                   'seed': 7,
-                  'max_iter': 2, #-1, # manual termination condition for short test
+                  'max_iter': 100, #-1, # manual termination condition for short test
                   'verbose': True}
 
 

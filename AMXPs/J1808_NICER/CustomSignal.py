c4a8ec83604f66d28a5de8e2436abd3ca417b075
Bas Dorsman <superbas1996@gmail.com>
2025-02-10 14:48:42 +0100

    disk and line are now separate in the photosphere

--- AMXPs/J1808_NICER/CustomSignal.py
+++ AMXPs/J1808_NICER/CustomSignal.py
@@ -91,7 +91,18 @@ class CustomSignal(xpsi.Signal):
                                               # allow_negative_background = self.allow_negative_background)#,
                                               #slim=-1.0) # default is skipping 10^89s, so some likelihood calculations are skipped
 
-        elif 'disk' in self.bkg or 'line' in self.bkg:
+        elif 'disk' in self.bkg:
+            self._phases += [np.copy(self._phases[0])]
+            
+            # fine as long as the disk has no phase
+            self._shifts = np.append(self._shifts, self._shifts[0])
+            
+            if 'line' in self.bkg:
+                self._phases += [np.copy(self._phases[0])]
+                
+                # fine as long as the line has no phase
+                self._shifts = np.append(self._shifts, self._shifts[0])
+       
             self.loglikelihood, self.expected_counts = \
                 poisson_likelihood_given_background(self._data.exposure_time, 
                                                     self._data.phases, 

f9b82a6a773969ac3c0a2ef155ec784853093ff2
Bas Dorsman <dorsman@int4.local.snellius.surf.nl>
2025-12-26 06:59:09 +0100

    snellius files and final STS run

---
+++ AMXPs/J1444/snellius_runs/run.sh
@@ -0,0 +1,24 @@
+export machine=snellius
+export num_energies=40 #60 #40
+export num_leaves=30 # 50 #30
+export sqrt_num_cells=50 #90 #50
+export num_rays=512
+export live_points=20
+export max_iter=1
+export run_type=sample
+export bkg=disk
+export support_factor=None
+export scenario=J1444_STU
+export poisson_noise=True
+export poisson_seed=42
+export fix_mass=False
+export sampler=multi
+export LABEL=test_analysis
+export eos_informed=False
+#export polarization=iqu
+export channel_min=100
+
+cd $HOME/xpsi-bas-fork/AMXPs/J1444/
+python STU.py
+cd $HOME/xpsi-bas-fork/AMXPs/J1444/snellius_runs/
+
